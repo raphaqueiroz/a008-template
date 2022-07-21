60171fdfce8c6f2517b0961fa028cd07bcf0495f
raphaqueiroz <ralphqueiros@gmail.com>
2022-07-20 21:36:42 -0300

    exercíio aula008

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,49 @@
-//CODIGO AQUI
+let saldo = 15
+
+saldo = saldo + 22.40
+console.log(saldo)
+
+multCalculo = 2*2+2
+console.log(multCalculo)
+
+let comparacaoIgual = 5 === 5
+console.log(comparacaoIgual)
+
+let comparacaoDiferente = 5 !== 5
+console.log(comparacaoDiferente)
+
+let comparacaoTipos = 5 !== "5"
+console.log(comparacaoTipos)
+
+let comp = "5" === "cinco"
+console.log(comp)
+
+// !== compara o valor e tipo.
+// != compara apenas o valor.
+
+ let tipos = typeof 5 === typeof 20
+console.log(tipos)
+
+let tipos2 = typeof "5" === typeof "cinco"
+
+const num1 = 7
+const num2 = 13
+let resultado
+
+resultado = num1 == num2
+console.log('O primeiro núero é igual o segundo?')
+console.log(resultado)
+
+resultado = num1 <= num2
+console.log('O primeiro numero é menor ou igual o segundo?')
+console.log(resultado)
+
+resultado = num1 > num2
+console.log('O primeiro número é maior que o segundo?')
+console.log(resultado)
+
+resultado = num1 < num2
+console.log('O primeiro número é menor que o segundo?')
+console.log(resultado)
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,19 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+const minhaIdade = parseInt(prompt('Qual sua idade?'))
+const idadeAmiga = parseInt(prompt('Qual a idade da sua amiga ou amigo?'))
+let comparacao
+
+comparacao = minhaIdade > idadeAmiga
+console.log("Sua idaade é maior que a do seu amigo?", comparacao)
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+
+!b && d
+(b && a) && !d
+a || d
+c && !d
